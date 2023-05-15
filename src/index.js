b6db1ab8c8d7fb5fa2bad046b45ecc25e475ecd8
Khoa <khoadinh2k@gmail.com>
2023-05-14 20:48:15 -0500

    added ability to play, skip, pause, and unpause music player

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const { Client, IntentsBitField } = require('discord.js');
 const ytdl = require('ytdl-core');
-// const scrapeIt = require('scrape-it');
 const { joinVoiceChannel, createAudioPlayer, createAudioResource } = require('@discordjs/voice');
 require('dotenv').config();
 
@@ -19,13 +18,73 @@ client.once('ready', (c) => {
 	console.log(`${c.user.tag} is ready!`);
 });
 
+const musicQueue = [];
+const musicTitleQueue = [];
+var musicTitleList = '';
+var currentSong = '';
+
+var isPlaying = false;
+
+const player = createAudioPlayer();
+
+async function handleMusicTitle() {
+	const info = await ytdl.getInfo(currentSong);
+	const title = info.videoDetails.title;
+	musicTitleQueue.push(title);
+
+	musicTitleQueue.forEach((titleList) => {
+		musicTitleList = musicTitleList + titleList + '\n';
+	});
+}
+
+async function playSong(message) {
+	const connection = await joinVoiceChannel({
+		channelId: message.member.voice.channel.id,
+		guildId: message.guild.id,
+		adapterCreator: message.guild.voiceAdapterCreator,
+	});
+	if (currentSong !== '') {
+		isPlaying = true;
+		const stream = ytdl(currentSong, { filter: 'audioonly' }, { quality: '94' });
+		const resource = createAudioResource(stream);
+
+		await player.play(resource);
+		connection.subscribe(player);
+	}
+	else {
+		message.reply('There are no songs in the queue.');
+	}
+}
+
+async function stopSong() {
+	await player.stop();
+}
+
+async function pauseSong() {
+	await player.pause();
+}
+
+async function resumeSong() {
+	await player.unpause();
+}
+
 client.on('messageCreate', async message => {
 	if (message.content.startsWith('!play')) {
-		const args = message.content.slice(5).trim().split(/ +/);
-		const url = args[0];
+		const args = message.content.slice(5).trim().split(/ +/) || '';
+		const url = args[0] || '';
 
 		if (!url) {
 			return message.reply('Please provide a video URL.');
+		}
+
+		const youtubeRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w-]+\?v=|embed\/|v\/)?)([\w-]+)(\S+)?$/;
+		if (!youtubeRegex.test(url)) {
+			return message.reply('Invalid YouTube URL.');
+		}
+
+		musicQueue.push(url);
+		if (!currentSong) {
+			currentSong = musicQueue.shift();
 		}
 
 		const voiceChannel = message.member.voice.channel;
@@ -35,99 +94,35 @@ client.on('messageCreate', async message => {
 		}
 
 		try {
-			const connection = await joinVoiceChannel({
-				channelId: message.member.voice.channel.id,
-				guildId: message.guild.id,
-				adapterCreator: message.guild.voiceAdapterCreator,
-			});
-
-			const stream = ytdl(url, { filter: 'audioonly' }, { quality: '94' });
-
-			const player = createAudioPlayer();
-			const resource = createAudioResource(stream);
-
-			// eslint-disable-next-line no-inner-declarations
-			async function play() {
-				await player.play(resource);
-				connection.subscribe(player);
+			if (!isPlaying) {
+				playSong(message);
 			}
-
-			play();
-
 		}
 		catch (error) {
 			console.error(error);
 		}
 	}
+
+	if (message.content.startsWith('!skip')) {
+		currentSong = musicQueue.shift();
+		if (!currentSong) {
+			message.reply('There are no songs in the queue.');
+			stopSong();
+			isPlaying = false;
+		}
+		else if (currentSong) {
+			message.reply(`Now playing: ${currentSong}`);
+			playSong(message);
+		}
+	}
+
+	if (message.content.startsWith('!pause')) {
+		pauseSong();
+	}
+
+	if (message.content.startsWith('!resume')) {
+		resumeSong();
+	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
-
-// client.on('message', async message => {
-// 	if (message.content.startsWith('!play')) {
-// 		const args = message.content.slice(5).trim().split(/ +/);
-// 		const url = args[0];
-
-// 		if (!url) {
-// 			return message.reply('Please provide a video URL.');
-// 		}
-
-// 		const voiceChannel = message.member.voice.channel;
-
-// 		if (!voiceChannel) {
-// 			return message.reply('You need to be in a voice channel to play music!');
-// 		}
-
-// 		let stream;
-
-// 		// Check if the URL is a YouTube link
-// 		if (ytdl.validateURL(url)) {
-// 			stream = ytdl(url, { filter: 'audioonly' });
-// 		}
-// 		else if (url.includes('fmovies')) {
-// 			try {
-// 				// Scrape the FMovies page to get the video URL
-// 				const { data } = await scrapeIt(url, {
-// 					video: {
-// 						selector: 'iframe#iframe-embed',
-// 						attr: 'src',
-// 					},
-// 				});
-
-// 				stream = data.video ? data.video : null;
-// 			}
-// 			catch (error) {
-// 				console.error(error);
-// 				return message.channel.send('Error retrieving video URL from FMovies.');
-// 			}
-// 		}
-// 		else {
-// 			return message.channel.send('Invalid video URL.');
-// 		}
-
-// 		try {
-// 			const connection = await voiceChannel.join();
-// 			const dispatcher = connection.play(stream);
-
-// 			dispatcher.on('start', () => {
-// 				message.channel.send(`Now playing: ${url}`);
-// 			});
-
-// 			dispatcher.on('finish', () => {
-// 				voiceChannel.leave();
-// 			});
-
-// 			dispatcher.on('error', error => {
-// 				console.error(error);
-// 				message.channel.send('Error playing video.');
-// 				voiceChannel.leave();
-// 			});
-// 		}
-// 		catch (error) {
-// 			console.error(error);
-// 			message.channel.send('Error joining voice channel.');
-// 		}
-// 	}
-// });
-
-// client.login(process.env.DISCORD_TOKEN);
